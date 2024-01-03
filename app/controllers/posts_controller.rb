d95eeec0b5651ecd72097a538c1c4238b443012c
maclen <maclen2007@yandex.ru>
2024-01-03 17:27:48 +0300

    Создание новой статьи пользователем

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,29 @@
 class PostsController < ApplicationController
 
+  before_action :authenticate_user!, only: [:new]
+
   def index
     @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = current_user.post.create(post_params)
+    flash[:notice] = "Статья была создана"
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def authenticate_user
+    redirect_to new_user_session_path if current_user.nil?
   end
 
 end
